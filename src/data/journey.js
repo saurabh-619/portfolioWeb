4b5cca46dfda1f8a7af4d100a3967fc4dbfbf271
Saurabh Bomble <57832707+saurabh-619@users.noreply.github.com>
2020-05-12 23:48:44 +0530

    Update journey.js

--- src/data/journey.js
+++ src/data/journey.js
@@ -18,7 +18,7 @@ const journey = {
   web: {
     topic: "Web Developement",
     text:
-      "In the second year of engineering, I saw some of the websites made by my batchmates, I found those very fascinating, intuitive, and thought I should also learn Web Development. So here my Web journey got started. I learned HTML, CSS, SASS, JS for Front-End, and Nodejs, Mongo Db for Back-End. This took me like 4-5 months. These were tough months, reading through documents, StackOverflow for errors. But still, I managed to create some interesting projects. I learned React Js which was a major bump in Design and speed of coding.",
+      "In the second year of engineering, I saw some of the websites made by my batchmates, I found those very fascinating, intuitive, and thought I should also learn Web Development. So here my Web journey got started. I learned HTML, CSS, SASS, JS for Front-End, and Nodejs, Mongo Db for Back-End. This took me like 4-5 months. Those were tough months, reading through documents, StackOverflow for errors. But still, I managed to create some interesting projects. I learned React Js which was a major bump in Design and speed of coding.",
     date: "2020",
     imageUrl:
       "https://raw.githubusercontent.com/saurabh-619/images/2f068da619b996fe313ea51d9b10c9af9774fadc/journey/webdeve.svg",
