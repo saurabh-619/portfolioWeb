b12912a0dbd561f7f8651c9e2b7586f93232c530
Saurabh Bomble <57832707+saurabh-619@users.noreply.github.com>
2020-05-20 22:42:21 +0530

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -48,7 +48,7 @@ const app = [
       "HomieGram is an Instagram clone that provides you all features like add , like, comment a post. You can also follow people. When a person like, comment a post, the person receives a notification of their device.",
     imageUrl:
       "https://raw.githubusercontent.com/saurabh-619/images/master/homiegramlogo.png",
-    url: "https://drive.google.com/open?id=1D9xOe7euRVMkDIg27gOPZAqnoJrvD0Tn",
+    url: "https://drive.google.com/open?id=1Er6L1-nxrbYMTdzoII8TgEkH1UbiMar2",
   },
 ];
 
